ec072f06e46c62fcafe8596e14b6bf266ff6c791
tmarufu <tatenda.marufu@ecocashholdings.co.zw>
2024-12-05 12:19:57 +0200

    add star clickable action

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -2,6 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+interface Item {
+  id: number;
+  title: string;
+  content: string;
+  isStarred: boolean;
+}
+
 @Component({
   selector: 'app-dashboard',
   standalone: true,
@@ -9,11 +16,29 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
+  items: Item[] = [
+    { id: 1, title:"Timbuk2 - 5 days ago", content: "item 1", isStarred: false },
+    { id: 2, title:"Timbuk2 - 5 days ago", content: "item 2", isStarred: false },
+    { id: 3, title:"Timbuk2 - 5 days ago",
+       content: "Item 3,Item 3, Item 3,Item 3,Item 3,Item 3",
+        isStarred: false },
+    { id: 4, title:"Timbuk2 - 5 days ago", content: "Item 4", isStarred: false },
+    { id: 5, title:"Timbuk2 - 5 days ago",
+       content: "Item 5,Item 5,Item 5,Item 5,Item 5,Item 5,Item 5,Item 5,Item 5,Item 5,Item 5,Item 5",
+        isStarred: false },
+    { id: 6,title:"Timbuk2 - 5 days ago",
+      content: "Item 5,Item 5,Item 5,Item 5,Item 5,Item 5",  isStarred: false }
+  ];
 
-  constructor() { }
+   selectedStars: boolean = false ;
 
-  ngOnInit() {
+
+   toggleStar(itemId: number) {
+    const item = this.items.find(item => item.id === itemId);
+    if (item) {
+      item.isStarred = !item.isStarred;
+    }
   }
 
 }
